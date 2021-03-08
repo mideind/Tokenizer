6653e3eedc66a46be6bc3dd6c2d390accbfe0a65
Vilhjalmur Thorsteinsson <vt@extrada.com>
2021-03-08 11:47:57 +0000

    Modified release.sh to call twine directly

--- release.sh
+++ release.sh
@@ -6,5 +6,5 @@ echo "Prepare and upload a new Tokenizer version"
 rm -rf build/*
 python setup.py bdist_wheel --universal
 python setup.py sdist
-python -m twine upload dist/tokenizer-$1*
+twine upload dist/tokenizer-$1*
 
